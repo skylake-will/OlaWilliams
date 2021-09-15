ba3cfd6b64e22784ca048bceb3f8cf4574a86611
Williams Silva <Williams Silva@GAMINGPC-1>
2021-09-15 11:09:41 -0300

    Adicionar arquivos de projeto.

---
+++ OláMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OláWilliams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Williams");
+        }
+    }
+}
